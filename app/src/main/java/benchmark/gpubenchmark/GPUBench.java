07be42ed1db00a22013f657148b7b196aed8e696
HabarovIM <habarovim@mer.ci.nsu.ru>
2022-05-13 11:35:48 +0700

    Xernya

--- app/src/main/java/benchmark/gpubenchmark/GPUBench.java
+++ app/src/main/java/benchmark/gpubenchmark/GPUBench.java
@@ -1,4 +1,62 @@
 package benchmark.gpubenchmark;
 
-public class GPUBench {
+import benchmark.IBenchmark;
+import database.Score;
+
+public class GPUBench implements IBenchmark {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void initialize(Long size) {
+
+    }
+
+    @Override
+    public void warmup() {
+
+    }
+
+    @Override
+    public void run(Object... param) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public long memInfo() {
+        return 0;
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void clean() {
+
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public Score getScore() {
+        return null;
+    }
+
+    @Override
+    public void compute() {
+
+    }
 }
